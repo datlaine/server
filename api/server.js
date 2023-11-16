9afc3095cfa9d46bf049d76c8002a2ed9e86a834
Lại Huỳnh Phát Đạt <136949840+datlaine@users.noreply.github.com>
2023-11-16 21:32:25 +0700

    Update server.js

--- api/server.js
+++ api/server.js
@@ -10,6 +10,11 @@ server.use(jsonServer.rewriter({
     '/api/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
 }))
+
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
+});
+
 server.use(router)
 server.listen(8000, () => {
     console.log('JSON Server is running')
